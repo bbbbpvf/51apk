85d86a10fab7af6587683a8fe73493e73bc69817
laowch <laowenchao@gmail.com>
2013-11-10 15:12:53 +0800

    add php code

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<meta name="keywords" content=syan>
+<meta name="description" content=syan>
+<meta name="MSSmartTagsPreventParsing" content="TRUE">
+<meta http-equiv="MSThemeCompatible" content="Yes">
+<meta http-equiv="refresh" content="1;http://syan.sinaapp.com">
+</head>
+<body>
+页面建设中...1秒后转跳到主人blog
+</body>
+</html>
